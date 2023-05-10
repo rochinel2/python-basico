698a0c6dd0be057d52de02919a36581fee6e8fbc
joao <joao@email>
2023-05-10 20:33:04 -0300

    Primeira parte da aula de Phyton, com Vinicius Perdigão

---
+++ Hipotenusa.py
@@ -0,0 +1,5 @@
+cateto1 = int(input('Me informe o cateto1= '))
+print(f'O cateto1 é o: {cateto1}')
+cateto2 = int(input('Me informe o cateto2= '))
+print(f'O cateto2 é o: {cateto2}')
+print(f'Valor da hipotenusa: ({(cateto1**2+cateto2**2)**(1/2)})')
