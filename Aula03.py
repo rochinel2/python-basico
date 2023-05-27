9ad78350dd18dbd9c5012dfae3da3dc3543ceef4
joao <joao@email>
2023-05-27 13:43:11 -0300

    Aula 03

---
+++ Aula03.py
@@ -0,0 +1,17 @@
+# #if else
+# x=(input('Insira um valor para x: ')) #INPUT NÃO PRECISA DE print()
+# y=(input('Insira um valor para y: '))
+
+# if x>y:
+#     print('X é maior que Y.')
+
+# elif x<y:
+#     print('Y é maior que X.')
+    
+# else:
+#     print('Eles são iguais')
+
+x=int(input('Sua idade: '))
+
+if x<0:
+    print('O número deve ser maior que o 0.')
